f3a08bbd8ff6e01aa5cf58de19b9db339b345b1b
Bartosz Matusiak <bartosz.matusiak@eastsoft.pl>
2025-04-23 21:51:02 +0200

    Fixing problem with js invoke

--- Shopify.Web/Pages/Checkout.razor.cs
+++ Shopify.Web/Pages/Checkout.razor.cs
@@ -1,23 +1,33 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using Shopify.Models.Dtos;
-using Shopify.Web.Services;
 using Shopify.Web.Services.Contracts;
 
 namespace Shopify.Web.Pages
 {
     public partial class Checkout : ComponentBase
     {
-        [Inject]public IJSRuntime Js { get; set; }
+        [Inject] public IJSRuntime Js { get; set; }
         protected IEnumerable<CartItemDto> ShoppingCartItems { get; set; }
         protected int TotalQty { get; set; }
         protected string PaymentDescription { get; set; }
         protected decimal PaymentAmount { get; set; }
-        [Inject]public IShoppingCartService ShoppingCartService { get; set; }
-        [Inject]public IOrderService OrderService { get; set; }
-
+        [Inject] public IShoppingCartService ShoppingCartService { get; set; }
+        [Inject] public IOrderService OrderService { get; set; }
+        private DotNetObjectReference<Checkout>? _dotNetRef;
 
         protected string DisplayButtons { get; set; } = "block";
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                // Create a DotNetObjectReference for this component
+                _dotNetRef = DotNetObjectReference.Create(this);
+                // Pass the DotNetObjectReference to the JavaScript function
+                await Js.InvokeVoidAsync("initPayPalButton", _dotNetRef);
+            }
+        }
 
         protected override async Task OnInitializedAsync()
         {
@@ -32,13 +42,11 @@ namespace Shopify.Web.Pages
                     PaymentAmount = ShoppingCartItems.Sum(p => p.TotalPrice);
                     TotalQty = ShoppingCartItems.Sum(p => p.ProductQuantity);
                     PaymentDescription = $"O_{Guid.Parse("79E9147F-44E3-4026-8BB6-061EF1CEFE4C")}_{orderGuid}";
-
                 }
                 else
                 {
                     DisplayButtons = "none";
                 }
-
             }
             catch (Exception)
             {
@@ -46,33 +54,23 @@ namespace Shopify.Web.Pages
             }
         }
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
+        [JSInvokable("CreateOrderAfterPayment")]
+        public async Task CreateOrderAfterPayment()
         {
-            try
-            {
-                if (firstRender)
-                {
-                    await Js.InvokeVoidAsync("initPayPalButton");
-                }
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-        }
-
-
-        protected async Task CreateOrder_Click()
-        {
+            Console.WriteLine("JSInvokable method called successfully!");
             try
             {
                 var createdOrder = await OrderService.CreateOrderAsync(ShoppingCartItems, Guid.Parse("79e9147f-44e3-4026-8bb6-061ef1cefe4c"), 1);
             }
             catch (Exception ex)
             {
-                throw;
+                Console.WriteLine($"Error in CreateOrderAfterPayment: {ex.Message}");
             }
+        }
+
+        public void Dispose()
+        {
+            _dotNetRef?.Dispose();
         }
     }
 }
